1bd111151a431bbf6086f6933308fcb3af18cc5c
Shamimreza82 <shamimrezaone@gmail.com>
2026-07-22 21:04:54 +0600

    added load seed

--- backend-api-finupsbd/src/seed.ts
+++ backend-api-finupsbd/src/seed.ts
@@ -1,9 +1,11 @@
 import { seedSuperAdmin, seedDemoUser } from './app/DB';
+import { seedLoans } from './app/DB/seed-loans';
 import { prisma } from './app';
 
 (async () => {
   await seedSuperAdmin();
   await seedDemoUser();
+  await seedLoans();
   await prisma.$disconnect();
 })().catch((e) => {
   console.error(e);
